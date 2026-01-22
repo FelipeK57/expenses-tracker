16cc4bcf588ef8b3f48cbe8e4912f8cef10d64b4
Kevin <kevinfelipebolanosorozco@gmail.com>
2026-01-22 17:26:10 -0500

    Refactor Home component to update recent transactions structure, enhance UI with income and expense cards, and integrate navigation buttons for month selection

--- src/app/features/views/Home.tsx
+++ src/app/features/views/Home.tsx
@@ -1,106 +1,117 @@
 import { Button, Card, CardBody } from "@heroui/react";
-import { Bus, ChevronRight, Popcorn, Utensils } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  TrendingDown,
+  TrendingUp,
+} from "lucide-react";
 import { NewIncome } from "../components/NewIncome";
 import { NewExpense } from "../components/NewExpense";
+import {
+  type Transaction,
+  expenseCategories,
+  incomeCategories,
+} from "@/app/schema";
 
 export const Home = () => {
-  const expensesByCategory = [
+  const recentTransactions: Transaction[] = [
     {
-      category: "Comida",
-      amount: 345.67,
-      icon: (
-        <div className="grid place-content-center size-10 bg-orange-400/20 rounded-xl">
-          <Utensils className="size-5 text-orange-400" />
-        </div>
-      ),
+      type: "income",
+      note: "Pago de nómina",
+      amount: 2000.0,
+      date: "2024-06-01",
+      category: "Salary",
     },
     {
-      category: "Transporte",
-      amount: 123.45,
-      icon: (
-        <div className="grid place-content-center size-10 bg-blue-400/20 rounded-xl">
-          <Bus className="size-5 text-blue-400" />
-        </div>
-      ),
+      type: "expense",
+      note: "Compra de comestibles",
+      amount: -150.75,
+      date: "2024-06-03",
+      category: "Food",
     },
     {
-      category: "Entretenimiento",
-      amount: 89.99,
-      icon: (
-        <div className="grid place-content-center size-10 bg-red-400/20 rounded-xl">
-          <Popcorn className="size-5 text-red-400" />
-        </div>
-      ),
+      type: "expense",
+      note: "Gasolina para el coche",
+      amount: -40.0,
+      date: "2024-06-05",
+      category: "Gasoline",
+    },
+    {
+      type: "expense",
+      note: "Cena en restaurante",
+      amount: -60.0,
+      date: "2024-06-07",
+      category: "Food",
+    },
+    {
+      type: "expense",
+      note: "Transporte público",
+      amount: -20.0,
+      date: "2024-06-08",
+      category: "Transport",
     },
   ];
 
-  const recentTransactions = [
-    {
-      reason: "Almuerzo",
-      amount: 15.67,
-      date: "2024-06-01",
-      category: "Comida",
-    },
-    {
-      reason: "Taxi",
-      amount: 23.45,
-      date: "2024-06-02",
-      category: "Transporte",
-    },
-    {
-      reason: "Cine",
-      amount: 12.99,
-      date: "2024-06-03",
-      category: "Entretenimiento",
-    },
-    {
-      reason: "Cena",
-      amount: 30.0,
-      date: "2024-06-04",
-      category: "Comida",
-    },
-    {
-      reason: "Bus",
-      amount: 10.0,
-      date: "2024-06-05",
-      category: "Transporte",
-    },
-  ];
+  // Obtenemos mes y año actuales
+  const month = new Date().getMonth();
+  const year = new Date().getFullYear();
+
+  const findIconByCategory = (
+    type: "income" | "expense",
+    categoryKey: string,
+  ) => {
+    const categories = type === "income" ? incomeCategories : expenseCategories;
+    const category = categories.find((cat) => cat.key === categoryKey);
+    return category ? category.icon : null;
+  };
 
   return (
     <main className="relative flex flex-col gap-2 p-4 h-full">
       <article className="flex flex-col gap-2">
-        <h1 className="text-center text-default-500">Total balance</h1>
-
-        <p className="text-center first-letter:g font-bold text-6xl py-2">
-          $12,345.67
-        </p>
-
-        <article className="space-y-2">
-          <div className="flex items-center justify-between w-full">
-            <p>Categorías del mes</p>
-            <Button
-              endContent={<ChevronRight className="size-5" />}
-              variant="light"
-            >
-              Ver todas
+        <article className="flex items-center justify-between">
+          <p>
+            Resumen de{" "}
+            {new Date(year, month).toLocaleString("es-ES", {
+              month: "long",
+              year: "numeric",
+            })}
+          </p>
+          <article className="flex items-center justify-center gap-2 mt-2">
+            <Button isIconOnly variant="light" size="sm">
+              <ChevronLeft className="size-5" />
             </Button>
-          </div>
-          <div className="flex gap-2 w-full overflow-x-auto pb-1">
-            {expensesByCategory.map(({ category, amount, icon }) => (
-              <Card key={category} className="min-w-1/2">
-                <CardBody>
-                  {icon}
-                  <p className="mt-4 font-semibold text-default-500">
-                    {category}
-                  </p>
-                  <p className="font-semibold text-xl mt-2">
-                    ${amount.toFixed(2)}
-                  </p>
-                </CardBody>
-              </Card>
-            ))}
-          </div>
+            <Button isIconOnly variant="light" size="sm" className="ml-2">
+              <ChevronRight className="size-5" />
+            </Button>
+          </article>
+        </article>
+
+        <div className="py-4">
+          <p className="text-sm text-center">Balance actual</p>
+          <p className="text-center first-letter:g font-bold text-5xl">
+            $51,889.00
+          </p>
+        </div>
+
+        <article className="grid grid-cols-2 gap-4">
+          <Card className="w-full">
+            <CardBody>
+              <div className="grid place-content-center size-10 bg-success-400/20 rounded-xl">
+                <TrendingUp className="size-5 text-success" />
+              </div>
+              <p className="mt-4 font-semibold text-default-500">Ingresos</p>
+              <p className="font-semibold text-xl mt-2">$102,012</p>
+            </CardBody>
+          </Card>
+          <Card className="w-full">
+            <CardBody>
+              <div className="grid place-content-center size-10 bg-danger-400/20 rounded-xl">
+                <TrendingDown className="size-5 text-danger" />
+              </div>
+              <p className="mt-4 font-semibold text-default-500">Gastos</p>
+              <p className="font-semibold text-xl mt-2">$50,123</p>
+            </CardBody>
+          </Card>
         </article>
       </article>
       <article className="flex flex-col gap-2 flex-1 min-h-0">
@@ -114,21 +125,34 @@ export const Home = () => {
           </Button>
         </div>
         <div className="flex-1 min-h-0 overflow-y-auto space-y-2">
-          {recentTransactions.map(
-            ({ reason, amount, date, category }, index) => (
-              <Card key={index}>
-                <CardBody className="flex justify-between">
-                  <div>
-                    <p className="font-semibold">{reason}</p>
-                    <p className="text-sm text-default-500">
-                      {date} · {category}
-                    </p>
-                  </div>
-                  <p className="font-semibold">${amount.toFixed(2)}</p>
-                </CardBody>
-              </Card>
-            ),
-          )}
+          {recentTransactions.map((transaction, index) => (
+            <Card key={index}>
+              <CardBody className="flex flex-row gap-4 items-center">
+                <div
+                  className={`grid place-content-center size-12 ${
+                    transaction.type === "income"
+                      ? "bg-success-400/20 text-success"
+                      : "bg-danger-400/20 text-danger"
+                  } rounded-xl mb-2`}
+                >
+                  {(() => {
+                    const Icon: any = findIconByCategory(
+                      transaction.type,
+                      transaction.category,
+                    ) as typeof Icon;
+                    return Icon ? <Icon className="size-6" /> : null;
+                  })()}
+                </div>
+                <div className="flex flex-col">
+                  <p className="font-semibold">
+                    ${transaction.amount.toFixed(2)}
+                  </p>
+                  <p className="text-sm text-default-500">{transaction.date}</p>
+                  {/* <p className="font-semibold">{transaction.note}</p> */}
+                </div>
+              </CardBody>
+            </Card>
+          ))}
         </div>
       </article>
       <div className="absolute bottom-4 right-4 flex flex-col gap-4">
